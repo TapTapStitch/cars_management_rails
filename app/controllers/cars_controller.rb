758bd4c93c11ead005ea0656659ed9eba844cfd4
TapTapStitch <91415410+TapTapStitch@users.noreply.github.com>
2023-02-27 18:27:02 +0200

    moved save request logic to service

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,9 +9,9 @@ class CarsController < ApplicationController
     return unless @search_request.valid?
 
     @cars = SearchQueryCars.new(Car.all, params).call
-    @number_of_cars = @cars.length.to_s
+    @total_cars_count = @cars.length
     @pagy, @cars = pagy(@cars, items: 5)
-    save_request if user_signed_in? && @cars.length.positive?
+    SaveSearchRequest.new(@search_request, current_user.id).call if user_signed_in? && @total_cars_count.positive?
   end
 
   def search; end
@@ -22,22 +22,7 @@ class CarsController < ApplicationController
 
   private
 
-  def save_request
-    user_search_request = SearchRequest.find_or_initialize_by(request_params(@search_request))
-    if user_search_request.persisted?
-      user_search_request.update(updated_at: Time.zone.now)
-    else
-      user_search_request.save
-    end
-  end
-
   def search_params
     params.permit(:make, :model, :price_from, :price_to, :year_from, :year_to, :odometer_from, :odometer_to)
-  end
-
-  def request_params(request)
-    { make: request.make, model: request.model, price_from: request.price_from, price_to: request.price_to,
-      year_from: request.year_from, year_to: request.year_to, odometer_from: request.odometer_from,
-      odometer_to: request.odometer_to, user_id: current_user.id }
   end
 end
