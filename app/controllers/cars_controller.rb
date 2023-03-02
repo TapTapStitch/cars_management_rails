9001dd9708eae9dc31051880ddabd7632df2dd61
TapTapStitch <91415410+TapTapStitch@users.noreply.github.com>
2023-03-02 11:10:42 +0200

    refactored way to display request errors (from  @search_request.errors.full_messages to flash.alert) its better way

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,12 +6,14 @@ class CarsController < ApplicationController
 
   def index
     @search_request = SearchRequest.new(search_params)
-    return unless @search_request.valid?
-
-    @cars = SearchQueryCars.new(Car.all, params).call
-    @total_cars_count = @cars.length
-    @pagy, @cars = pagy(@cars, items: 5)
-    SaveSearchRequest.new(@search_request, current_user.id).call if user_signed_in? && @total_cars_count.positive?
+    if @search_request.valid?
+      @cars = SearchQueryCars.new(Car.all, params).call
+      @total_cars_count = @cars.length
+      @pagy, @cars = pagy(@cars, items: 5)
+      save_search_request if @total_cars_count.positive?
+    else
+      display_errors
+    end
   end
 
   def search; end
@@ -21,6 +23,15 @@ class CarsController < ApplicationController
   end
 
   private
+
+  def save_search_request
+    SaveSearchRequest.new(@search_request, current_user.id).call if user_signed_in?
+  end
+
+  def display_errors
+    flash.alert = @search_request.errors.full_messages.join('<br>')
+    redirect_to search_cars_path
+  end
 
   def search_params
     params.permit(:make, :model, :price_from, :price_to, :year_from, :year_to, :odometer_from, :odometer_to)
--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -2,9 +2,10 @@
 
 module CarsHelper
   include Pagy::Frontend
+
   def sort_link(sort_order, title)
     link_to title,
             cars_path(params.merge(sort: sort_order).permit(:make, :model, :price_from, :price_to, :year_from, :year_to,
-                                                            :odometer_from, :odometer_to, :sort))
+                                                            :odometer_from, :odometer_to, :sort, :commit))
   end
 end
