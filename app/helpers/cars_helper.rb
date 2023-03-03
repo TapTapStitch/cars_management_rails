d9798871d489272522e08d185f2aff2fb1af785a
TapTapStitch <91415410+TapTapStitch@users.noreply.github.com>
2023-03-03 16:02:05 +0200

    fix

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -6,6 +6,6 @@ module CarsHelper
   def sort_link(sort_order, title)
     link_to title,
             cars_path(params.merge(sort: sort_order).permit(:make, :model, :price_from, :price_to, :year_from, :year_to,
-                                                            :odometer_from, :odometer_to, :sort, :commit))
+                                                            :odometer_from, :odometer_to, :sort, :commit, :page))
   end
 end
